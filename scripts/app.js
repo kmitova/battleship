5dd778432b58a9ba41d43815220d45a6ac84f628
kmitova <96522051+kmitova@users.noreply.github.com>
2022-09-16 16:09:58 +0300

    functionality for generating grid for player and computer

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,32 @@
-console.log("testing");
+const playerGrid = document.querySelector(".player-grid");
+const computerGrid = document.querySelector(".computer-grid");
 
-function sum(a, b) {
-  return a + b;
+function generatePlayerGrid() {
+  playerGrid.style.setProperty("--grid-rows", 10);
+  playerGrid.style.setProperty("--grid-cols", 10);
+  let i = 0;
+  for (let cell = 0; cell < 10 * 10; cell++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("id", i);
+    playerGrid.appendChild(cell).className = "player-grid-item";
+    i++;
+    console.log(i);
+  }
 }
-export { sum };
+
+function generateComputerGrid() {
+  computerGrid.style.setProperty("--grid-rows", 10);
+  computerGrid.style.setProperty("--grid-cols", 10);
+  let i = 100;
+  for (let cell = 0; cell < 10 * 10; cell++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("id", i);
+    computerGrid.appendChild(cell).className = "computer-grid-item";
+    i++;
+    console.log(i);
+  }
+}
+// for player grid: id from 1 to 100
+generatePlayerGrid();
+// for computer grid: id from 101 to 201
+generateComputerGrid();
