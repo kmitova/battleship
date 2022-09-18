0c4a0d5afb72fe674fae08a711be72828d035feb
kmitova <96522051+kmitova@users.noreply.github.com>
2022-09-18 16:38:55 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -51,33 +51,148 @@ const playerCells = Array.from(document.querySelectorAll(".player-grid-item"));
 // });
 
 // CRUISER 3 FIELDS
+// playerCells.forEach((cell) => {
+//   cell.addEventListener("click", () => {
+//     let currentId = cell.id;
+//     console.log(cell.id);
+//     let nextId;
+//     if (currentId % 10 === 0) {
+//       nextId = Number(currentId) - 1;
+//     } else {
+//       nextId = Number(currentId) + 1;
+//     }
+//     console.log(nextId);
+//     let nextEl = document.getElementById(nextId);
+//     // console.log(nextEl);
+//     cell.style.backgroundColor = "red";
+//     nextEl.style.backgroundColor = "red";
+
+//     let nextId2;
+//     if (nextId % 10 === 0) {
+//       nextId2 = Number(nextId) - 2;
+//     } else {
+//       nextId2 = Number(nextId) + 1;
+//     }
+//     let nextEl2 = document.getElementById(nextId2);
+//     console.log(nextId2);
+//     nextEl2.style.backgroundColor = "red";
+//   });
+// });
+
+// SUBMARINE 3 FIELDS
+// playerCells.forEach((cell) => {
+//   cell.addEventListener("click", () => {
+//     let currentId = cell.id;
+//     console.log(cell.id);
+//     let nextId;
+//     if (currentId % 10 === 0) {
+//       nextId = Number(currentId) - 1;
+//     } else {
+//       nextId = Number(currentId) + 1;
+//     }
+//     console.log(nextId);
+//     let nextEl = document.getElementById(nextId);
+//     // console.log(nextEl);
+//     cell.style.backgroundColor = "red";
+//     nextEl.style.backgroundColor = "red";
+
+//     let nextId2;
+//     if (nextId % 10 === 0) {
+//       nextId2 = Number(nextId) - 2;
+//     } else {
+//       nextId2 = Number(nextId) + 1;
+//     }
+//     let nextEl2 = document.getElementById(nextId2);
+//     console.log(nextId2);
+//     nextEl2.style.backgroundColor = "red";
+//   });
+// });
+
+// BATTLESHIP 4 FIELDS
 playerCells.forEach((cell) => {
   cell.addEventListener("click", () => {
     let currentId = cell.id;
-    console.log(cell.id);
+    console.log("current: " + cell.id);
     let nextId;
+    let nextId2;
+    let nextId3;
     if (currentId % 10 === 0) {
-      nextId = Number(currentId) - 1;
+      nextId = currentId - 1;
+      nextId2 = currentId - 2;
+      nextId3 = currentId - 3;
+    } else if (currentId % 10 === 9) {
+      nextId = currentId - 1;
+      nextId2 = currentId - 2;
+      nextId3 = currentId - 3;
+    } else if (currentId % 10 === 8) {
+      nextId = currentId - 1;
+      nextId2 = currentId - 2;
+      nextId3 = currentId - 3;
     } else {
       nextId = Number(currentId) + 1;
+      nextId2 = Number(currentId) + 2;
+      nextId3 = Number(currentId) + 3;
     }
-    console.log(nextId);
+    console.log(currentId, nextId, nextId2, nextId3);
     let nextEl = document.getElementById(nextId);
-    // console.log(nextEl);
+    let nextEl2 = document.getElementById(nextId2);
+    let nextEl3 = document.getElementById(nextId3);
+
     cell.style.backgroundColor = "red";
     nextEl.style.backgroundColor = "red";
-
-    let nextId2;
-    if (currentId % 10 === 0) {
-      nextId2 = Number(nextId) - 1;
-    } else {
-      nextId2 = Number(nextId) + 1;
-    }
-    let nextEl2 = document.getElementById(nextId2);
-    console.log(nextId2);
     nextEl2.style.backgroundColor = "red";
+    nextEl3.style.backgroundColor = "red";
   });
 });
+
+// CARRIER  FIELDS 5
+// playerCells.forEach((cell) => {
+//   cell.addEventListener("click", () => {
+//     let currentId = cell.id;
+//     console.log("current: " + cell.id);
+//     let nextId;
+//     let nextId2;
+//     let nextId3;
+//     let nextId4;
+//     if (currentId % 10 === 0) {
+//       nextId = currentId - 1;
+//       nextId2 = currentId - 2;
+//       nextId3 = currentId - 3;
+//       nextId4 = currentId - 4;
+//     } else if (currentId % 10 === 9) {
+//       nextId = currentId - 1;
+//       nextId2 = currentId - 2;
+//       nextId3 = currentId - 3;
+//       nextId4 = currentId - 4;
+//     } else if (currentId % 10 === 8) {
+//       nextId = currentId - 1;
+//       nextId2 = currentId - 2;
+//       nextId3 = currentId - 3;
+//       nextId4 = currentId - 4;
+//     } else if (currentId % 10 === 7) {
+//       nextId = currentId - 1;
+//       nextId2 = currentId - 2;
+//       nextId3 = currentId - 3;
+//       nextId4 = currentId - 4;
+//     } else {
+//       nextId = Number(currentId) + 1;
+//       nextId2 = Number(currentId) + 2;
+//       nextId3 = Number(currentId) + 3;
+//       nextId4 = Number(currentId) + 4;
+//     }
+//     console.log(currentId, nextId, nextId2, nextId3, nextId4);
+//     let nextEl = document.getElementById(nextId);
+//     let nextEl2 = document.getElementById(nextId2);
+//     let nextEl3 = document.getElementById(nextId3);
+//     let nextEl4 = document.getElementById(nextId4);
+
+//     cell.style.backgroundColor = "red";
+//     nextEl.style.backgroundColor = "red";
+//     nextEl2.style.backgroundColor = "red";
+//     nextEl3.style.backgroundColor = "red";
+//     nextEl4.style.backgroundColor = "red";
+//   });
+// });
 
 // playerCells.forEach((cell) => {
 //   cell.addEventListener("mouseover", (e) => {
